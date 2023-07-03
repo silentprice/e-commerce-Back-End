4d87ef67c304a86d4e16896c393b2610f6af84f1
Nick <nprice0502@yahoo.com>
2023-07-03 12:45:05 -0400

    updated routes and synced sequelize in the server.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{ model: Category }, { model: Tag }],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.status(500).json(err));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{ model: Category }, {model: Tag}],
+  })
+  .then((product) => {
+    if (!product) {
+      res.status(404).json({ message: 'No product with this id!' });
+        return;
+    }
+    res.json(product);
+  })
+  .catch((err) => res.status(500).json(err));
 });
 
 // create new product
@@ -90,7 +104,17 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id },
+  })
+    .then((product) => {
+      if (!product) {
+        res.status(404).json({ message: 'No product with this id!' });
+        return;
+      }
+      res.json({ message: 'Successfully deleted product!!' });
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }],
+  })
+  .then((tags) => res.json(tags))
+  .catch((err) =>res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: 'No tag with this id!' });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then((tag) => {
+      if (!tag[0]) {
+        res.status(404).json({ message: 'No tag with this id!' });
+        return;
+      }
+      res.json({ message: 'Successfully updated tag!!' });
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: 'No tag with this id!' });
+        return;
+      }
+      res.json({ message: 'Successfully deleted tag!!' });
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
